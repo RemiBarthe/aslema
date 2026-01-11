04f5f813a22ea11c4af6d537c4e1ed362946532f
RemiBarthe <barthe.rem@gmail.com>
2026-01-12 00:32:59 +0100

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from "./components/ui/button/Button.vue";
+</script>
+
+<template>
+  <h2 class="font-heading text-2xl font-bold">Toto super app</h2>
+
+  <Button> Click me </Button>
+</template>
+
+<style scoped></style>
